6876223c3ea294a9c6bab9a578507fff588a0278
buhtib <2418531916@qq.com>
2018-10-29 14:59:18 +0800

    完成第一页 音乐 中间文字 logo部分

--- js/quechao.js
+++ js/quechao.js
@@ -2,12 +2,12 @@ var animate = (function() {
 
     return {
         init:function() {
-            this.response_change( )
            
             this.event()
         },
         event:function() {
             var _this = this;
+            // 音乐播放功能
             $('.music').click(function() {
                 $(this).toggleClass('active');
                 if($(this).attr('class') !== 'music') {
@@ -16,16 +16,8 @@ var animate = (function() {
                     $(this).children('audio')[0].play()
                 }
             })
-        },
-        // 每页swiper响应宽高变化
-        response_change:function() {
-            // function change() {
-            //     var clientW = screen.availWidth;
-            //     var clientH = screen.availHeight;
-            //     $('.swiper-container').css({width: clientW, height: clientH})
-            // }
-            // window.addEventListener('load', change)
-            // window.addEventListener('resize', change)
         }
+       
+        
     }
 }())
