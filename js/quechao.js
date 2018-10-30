c497cf57814cfde041f52ff133c1e15f4844261d
buhtib <2418531916@qq.com>
2018-10-30 20:32:44 +0800

    完成第四页

--- js/quechao.js
+++ js/quechao.js
@@ -2,8 +2,12 @@ var animate = (function() {
 
     return {
         init:function() {
-           
+            // 自动播放
+            $.ready(function () {
+                $('audio')[0].play()
+            })
             this.event()
+            this.swiperStart();
         },
         event:function() {
             var _this = this;
@@ -16,8 +20,24 @@ var animate = (function() {
                     $(this).children('audio')[0].play()
                 }
             })
+        },
+        // 轮播图
+        swiperStart:function() {
+            var mySwiper = new Swiper('.swiper-container', {
+                direction: 'vertical',
+                mousewheel: true,
+                initialSlide:3,
+                on: {
+                    init: function () {
+                        swiperAnimateCache(this); //隐藏动画元素 
+                        swiperAnimate(this); //初始化完成开始动画
+                    },
+                    slideChangeTransitionEnd: function () {
+                        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+                    }
+                }
+            });
         }
-       
         
     }
 }())
